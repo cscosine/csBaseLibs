99aceae0867613f7f657e5f884c0ed8328330afe
cscosine-dev <cscosine@gmail.com>
2026-06-28 16:49:56 +0200

    add workflow

--- csBaseLibs.py
+++ csBaseLibs.py
@@ -16,7 +16,19 @@ from csorchestrator.domain.orchestrator.workflow_config import (
     ReleaseCreationOnTagConfig,
 )
 from csorchestrator.domain.context.context_os_architecture import OS
-from csorchestrator.domain.context.context_os_architecture import UBUNTU_STRING_PREFIX
+from csorchestrator.domain.context.context_os_architecture import (
+    UBUNTU_STRING_PREFIX,
+    UBUNTU_VERSIONS,
+)
+
+from csorchestrator.domain.context.context_os_architecture_compiler_generator import (
+    ContextOsArchitectureCompilerGenerator,
+)
+from csorchestrator.domain.context.context_compiler_generator import (
+    ContextCompilerGenerator,
+    Compiler,
+    GeneratorWithType,
+)
 
 from csorchestrator.frontend.cscmake_presets.supported_variants import (
     BuildConfig,
@@ -37,6 +49,9 @@ from csorchestrator.frontend.step.step_upload_artifacts import (
     create_artifact_prefix_from_orchestrator_name_version,
 )
 from csorchestrator.frontend.step.step_custom_command import StepInstallAptPackages
+from csorchestrator.frontend.step.step_get_precompiled_lib_github import (
+    StepGetPrecompiledLibGithub,
+)
 
 from csorchestrator.frontend.local_execution.step_utils import (
     StepExecuteOnlyOncePerMatrix,
@@ -56,6 +71,7 @@ def create_orchestrator() -> OptionalOrchestratorWithReport:
 
     base_target_dir = Path("workspace")
     base_install_dir = base_target_dir / Path("install")
+    base_libs_dir = base_target_dir / Path("libs")
     common_repo_ref = "dev"
 
     repos: dict[str, None | BuildConfig] = {
@@ -68,6 +84,11 @@ def create_orchestrator() -> OptionalOrchestratorWithReport:
 
     o = create_orchestrator_factory_all_supported_cases(
         name="csBaseLibs", version="0.1.0", execution_matrix_name="orchestrator-matrix"
+    )
+
+    report.append_warning("SKIPPING FIRST MATRIX ENTRY, FIX ME")
+    o.execution_matrix.os_architecture_compiler_generator_list = (
+        o.execution_matrix.os_architecture_compiler_generator_list[1:]
     )
 
     o.wf_config = WorkflowConfig(
@@ -127,52 +148,80 @@ def create_orchestrator() -> OptionalOrchestratorWithReport:
 
     # ----------------------------------------------------------------
     p = o.create_phase("Get Precompiled Libraries")
-    # list_3rdPartyBaseLibs = [
-    #    "eigen3",
-    #    "fmt",
-    #    "fmt-eigen",
-    #    "cpptrace",
-    #    "magic_enum",
-    #    "libassert",
-    #    "tclap",
-    #    "Catch2",
-    #    "pipes",
-    #    "NamedType",
-    #    "tl-optional",
-    #    "tl-expected",
-    # ]
-    #
-    # for lib in list_3rdPartyBaseLibs:
-    #    csGetPrecompiledLib(
-    #        base_url=repo_https_url + "cscosine",
-    #        repo="3rdPartyBaseLibs",
-    #        libName=lib,
-    #        version="v0.1.0-rc2",
-    #        libs_os_presets=libs_os_presets,
-    #    )
-    #
-    # qt_presets_mapping: Dict[str, Dict[str, str]] = {
-    #    "linux": {
-    #        "linux-ninja": "linux-gcc",
-    #        "linux-ninja-multi-config-clang": "linux-gcc",
-    #    },
-    #    "windows": {
-    #        "msvc2022-x64": "windows-msvc2022-x64",
-    #        "msvc2022-x64-LLVM": "windows-msvc2022-x64",
-    #    },
-    # }
-    #
-    # csGetPrecompiledLib(
-    #    base_url=repo_https_url + "cscosine",
-    #    repo="csQt6",
-    #    libName="qt6",
-    #    version="v6.10.2",
-    #    libs_os_presets=libs_os_presets,
-    #    presets_mapping=qt_presets_mapping,
-    # )
-    #
-    # ----------------------------------------------------------------
 
+    list_3rdPartyBaseLibs: dict[str, str] = {
+        "eigen3": "3.4.0",
+        "fmt": "11.2.1",
+        "fmt-eigen": "1.0.0",
+        "cpptrace": "0.8.3",
+        "magic_enum": "0.9.7",
+        "libassert": "2.1.5",
+        "tclap": "1.0.0",
+        "Catch2": "3.10.0",
+        "pipes": "0.0.1",
+        "NamedType": "1.1.0",
+        "tl-expected": "1.2.0",
+        "tl-optional": "1.0.0",
+    }
+
+    for lib_name, lib_version in list_3rdPartyBaseLibs.items():
+        p.add_step(
+            StepGetPrecompiledLibGithub(
+                name=f"Get Precompiled Lib {lib_name} from 3rdPartyBaseLibs",
+                description="get precompiled lib from github release",
+                base_url=StepGetPrecompiledLibGithub.GITHUB_BASE_URL_HTTPS,
+                org="cscosine",
+                project_name="3rdPartyBaseLibs",
+                project_tag="v0.1.0-test",
+                lib_name=lib_name,
+                lib_version=lib_version,
+                base_libs_dir=base_libs_dir,
+            )
+        )
+
+    def qt6_mapping(
+        context: ContextOsArchitectureCompilerGenerator,
+    ) -> ContextOsArchitectureCompilerGenerator | None:
+        if context.context_os_architecture.os == OS.LINUX:
+            if (
+                context.context_os_architecture.os_version
+                == UBUNTU_VERSIONS.UBUNTU_22_04.value
+                or context.context_os_architecture.os_version
+                == UBUNTU_VERSIONS.UBUNTU_24_04.value
+            ):
+                newContext = context
+                newContext.context_compiler_generator = ContextCompilerGenerator(
+                    compiler_family=Compiler.GCC,
+                    compiler_version=ContextCompilerGenerator.COMPILER_VERSION_DEFAULT,
+                    build_generator=GeneratorWithType.NINJA,
+                )
+                return newContext
+        elif context.context_os_architecture.os == OS.WINDOWS:
+            newContext = context
+            newContext.context_compiler_generator = ContextCompilerGenerator(
+                compiler_family=Compiler.MSVC,
+                compiler_version=ContextCompilerGenerator.COMPILER_VERSION_MSVC_2022_17,
+                build_generator=GeneratorWithType.NINJA,
+            )
+            return newContext
+        return None
+
+    p.add_step(
+        StepGetPrecompiledLibGithub(
+            name="Get Precompiled Lib qt6",
+            description="get precompiled lib from github release",
+            base_url=StepGetPrecompiledLibGithub.GITHUB_BASE_URL_HTTPS,
+            org="cscosine",
+            project_name="csQt6",
+            project_tag="v6.11.1",
+            lib_name="qt6",
+            lib_version="v6.11.1",
+            base_libs_dir=base_libs_dir,
+            mapping_function=qt6_mapping,
+        )
+    )
+
+    # ----------------------------------------------------------------
     p = o.create_phase("Configure-Build-Test-Install")
     for repo, config in repos.items():
         if config is not None:
